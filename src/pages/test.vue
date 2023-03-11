7eaf404604b86fd0ccbb4a86792bef5368d658e5
Teerachai Laothong <teerachai@perfenterprise.com>
2023-03-11 18:57:14 +0700

    Layout of test list

--- src/pages/test.vue
+++ src/pages/test.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 import { collection, addDoc, deleteDoc, doc } from '@firebase/firestore';
+import { initModals } from 'flowbite'
 
 const db = useFirestore();
 const fireCollection = collection(db, 'products');
@@ -17,6 +18,11 @@ const addProduct = async () => {
 }
 
 const deleteProduct = (id: string) => deleteDoc(doc(db, 'products', id))
+
+// initialize components based on data attribute selectors
+onMounted(() => {
+  initModals();
+})
 </script>
 
 <template>
@@ -25,7 +31,7 @@ const deleteProduct = (id: string) => deleteDoc(doc(db, 'products', id))
     tabindex="-1"
     aria-hidden="true"
     class="hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-modal md:h-full">
-    <div class="relative p-4 w-full max-w-2xl h-full md:h-auto">
+    <div class="relative p-4 w-full max-w-3xl h-full md:h-auto">
       <!-- Modal content -->
       <div class="relative p-4 bg-white rounded-lg shadow dark:bg-gray-800 sm:p-5">
         <!-- Modal header -->
