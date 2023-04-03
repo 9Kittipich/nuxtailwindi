a3f092f3dc732b02263b0b9c5838b2e7b163eced
jane99j <jaynerrch@gmail.com>
2023-04-03 14:11:35 +0700

    t

---
+++ src/pages/room.vue
@@ -0,0 +1,43 @@
+<template>
+    <section class="flex items-center bg-gray-50 dark:bg-gray-900 p-5 sm:p-5">
+        <div class="w-full max-w-screen-xl px-4 mx-auto lg:px-12">
+            <!-- Start coding here -->
+            <a href="#"
+                class="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
+               
+                    <h1 class="text-lg font-semibold text-gray-900 dark:text-white">101</h1>
+                
+                <figcaption class="flex items-center space-x-3">
+                    <div class="space-y-0.5 font-medium dark:text-white text-left">
+                        <div>Joseph McFall</div>
+                        <div class="text-sm text-gray-500 dark:text-gray-400">สถานะห้องพัก</div>
+                    </div>
+                    <div></div>
+                    <svg class="items-right" width="24%" height="24%" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg" 
+                        aria-hidden="true">
+                        <path clip-rule="evenodd" fill-rule="evenodd"
+                            d="M10 9a3 3 0 100-6 3 3 0 000 6zm-7 9a7 7 0 1114 0H3z"></path>
+                    </svg>
+                </figcaption>
+
+            </a>
+
+        </div>
+        
+<div class="max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+    <a href="#">
+        <h5 class="mb-2 text-2xl font-semibold tracking-tight text-gray-900 dark:text-white">101</h5>
+    </a>
+    <p class="mb-3 font-normal text-gray-500 dark:text-gray-400">ว่าง</p>
+    <svg class="w-10 h-10 mb-2 text-gray-500 dark:text-gray-400" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M5 5a3 3 0 015-2.236A3 3 0 0114.83 6H16a2 2 0 110 4h-5V9a1 1 0 10-2 0v1H4a2 2 0 110-4h1.17C5.06 5.687 5 5.35 5 5zm4 1V5a1 1 0 10-1 1h1zm3 0a1 1 0 10-1-1v1h1z" clip-rule="evenodd"></path><path d="M9 11H3v5a2 2 0 002 2h4v-7zM11 18h4a2 2 0 002-2v-5h-6v7z"></path></svg>
+</div>
+
+
+    </section>
+    
+</template>
+
+
+
+
+
