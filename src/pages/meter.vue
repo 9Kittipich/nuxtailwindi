d814a2596e299988cf39c73a2c1214d632e86d15
Rocky0191 <107449941+Rocky0191@users.noreply.github.com>
2023-04-05 10:36:53 +0700

    update Pagemeter

--- src/pages/meter.vue
+++ src/pages/meter.vue
@@ -22,7 +22,7 @@
                         </form>
                     </div>
                     <NuxtLink to='/meterwater'><button type="button"
-                            class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-15 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">ค่าน้ำ</button>
+                            class="text-white bg-green-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-15 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">ค่าน้ำ</button>
                     </NuxtLink>
                 </div>
                 <div class="overflow-x-auto">
