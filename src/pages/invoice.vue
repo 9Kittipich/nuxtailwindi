e0e0ee571a29c514325c8b7905e9f106ababe9ff
zxc2040 <107449746+zxc2040@users.noreply.github.com>
2023-04-05 10:47:32 +0700

    [commit] testBranch

--- src/pages/invoice.vue
+++ src/pages/invoice.vue
@@ -4,9 +4,7 @@
             <div class="bg-white dark:bg-gray-800 relative shadow-md sm:rounded-lg overflow-hidden">
                 <div class="flex flex-col md:flex-row items-center justify-between space-y-3 md:space-y-0 md:space-x-4 p-4">
                     <div class="w-full md:w-1/2 ">
-
                         <label for="simple-search">ใบแจ้งหนี้</label>
-
                     </div>
 
                     <NuxtLink to='/successfulPayment'><button type="button"
--- src/pages/successfulPayment.vue
+++ src/pages/successfulPayment.vue
@@ -10,7 +10,7 @@
                     </div>
 
                     <NuxtLink to='/invoice'><button type="button"
-                            class="text-white bg-red-500 hover:bg-red-400 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-15 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">ค่างชำระ</button>
+                            class="text-white bg-red-500 hover:bg-red -400 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-15 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">ค่างชำระ</button>
                     </NuxtLink>
                 </div>
                 <div class="overflow-x-auto">
